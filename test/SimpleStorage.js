463eda43c38d23d9bb6c0258e4577572439e62a6
Achintya0502 <achintyapatel55555@gmail.com>
2020-02-25 00:21:12 +0530

    updated tests

--- test/SimpleStorage.js
+++ test/SimpleStorage.js
@@ -1,10 +1,31 @@
 const SimpleStorage = artifacts.require('SimpleStorage')
 
 contract('SimpleStorage', () => {
-    it('Should set the value of data variable', async () => {
-        const simpleStorage = await SimpleStorage.deployed();
-        await simpleStorage.set(1);
-        const result = await simpleStorage.get();
-        assert(result === 1);
+    let simpleStorage = null;
+    before(async () => {
+        simpleStorage = await SimpleStorage.deployed();
+    });
+
+    it('Should add an element to ids array', async () => {
+        await simpleStorage.add(10);
+        const result = await simpleStorage.ids(0);
+        assert(result.toNumber() === 10);
+    });
+
+    it('Should get an element of ids array', async () => {
+        await simpleStorage.add(20);
+        const result = await simpleStorage.get(1);
+        assert(result.toNumber() === 20);
+    });
+
+    it('Should get ids array', async () => {
+        const result = await simpleStorage.getAll();
+        const ids = result.map(id => id.toNumber());
+        assert.deepEqual(ids,[10, 20]);
+    });
+
+    it('Should get length of ids array', async () => {
+        const result = await simpleStorage.length();
+        assert(result.toNumber() === 2);
     });
 });
